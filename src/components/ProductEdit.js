106c95381e87cdb43daa9b97ce600811e6a1358f
Anthony Campos <nythocampos@gmail.com>
2021-12-20 06:02:10 -0500

    productEdit was created

--- src/components/ProductEdit.js
+++ src/components/ProductEdit.js
@@ -9,29 +9,40 @@ function ProductEdit(props) {
   return (
     <Modal isOpen={props.isOpen} onClose={props.onClose}>
       <section>
-        <div className="productDetails">
-          <div className="productDetails__img">
+        <div className="productEdit">
+          <div className="productEdit__img">
             <img src={userImg} alt="" />
           </div>
-          <div className="productDetails__info">
-            <p>ID: No se cambia</p>
+          <div className="productEdit__info">
+            <p>ID:</p>
+            <input type="text" />
             <hr />
             <p>Nombre:</p>
+            <input type="text" />
             <hr />
             <p>Cantidad:</p>
-            <hr />
-            <p>Estado:</p>
+            <input type="text" />
             <hr />
             <p>Tipo:</p>
+            <input type="text" />
             <hr />
-            <p>Articulo:</p>
+            <p>Articulo de temporada:</p>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+
             <hr />
             <p>Descuento:</p>
+            <input type="text" />
             <hr />
             <p>Descripcion:</p>
+            <input type="text" />
             <hr />
           </div>
-          <div className="productDetails__options">
+          <div className="productEdit__options">
             <button>Editar producto</button>
             <button onClick={props.onClose}>Cancelar</button>
           </div>
